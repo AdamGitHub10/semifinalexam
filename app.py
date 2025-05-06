ec4c343451fba8720ee736d07f0d21cfcab71567
AdamGitHub10 <kenthpacas@gmail.com>
2025-05-06 23:37:31 +0800

    last1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Adam Karl Sansan ,br> BSIT 3-2nd 25 ,<br> System Integration and Architecture 1 <br> Semi Final "
+    return "Adam Karl Sansan ,<br> BSIT 3-2nd 25 ,<br> System Integration and Architecture 1 <br> Semi Final "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
