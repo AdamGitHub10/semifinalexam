671530fc3afc48c004d372201b48debd0c9a1549
AdamGitHub10 <kenthpacas@gmail.com>
2025-05-06 22:37:26 +0800

    2nd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Adam karlsansan"
+    return "Adam karl Sansan <br> BSIT-2A 2nd 25 <br> System Integration and Achitecture 1 <br> Semi Final Exam <br>"
 
 if __name__ == '__main__':
     app.run(debug=True)
